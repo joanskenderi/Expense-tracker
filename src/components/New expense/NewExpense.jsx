427d45e11420cf51bd8246add0c9301b1bb5a078
Joan Skënderi <jskenderi@outlook.com>
2022-11-07 22:22:32 +0100

    Final Commit🔥

---
+++ src/components/New expense/NewExpense.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHanlder = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString,
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHanlder} />
+    </div>
+  );
+};
+
+export default NewExpense;
