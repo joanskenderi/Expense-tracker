3026bb78d2704757e818a58f5c19d76a5148f090
Joan Skënderi <jskenderi@outlook.com>
2022-11-08 13:48:27 +0100

    Commit🔥

--- src/components/New expense/NewExpense.jsx
+++ src/components/New expense/NewExpense.jsx
@@ -4,17 +4,17 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const saveExpenseDataHanlder = (enteredExpenseData) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString,
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHanlder} />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
